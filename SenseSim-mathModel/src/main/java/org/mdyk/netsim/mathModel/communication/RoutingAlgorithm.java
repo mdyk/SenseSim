a2c47051306bbad2d996892ff820427f626d6991
mdyk <mdyk@wat.edu.pl>
2015-02-12 10:50:02 +0100

    Zablokowanie snapshota DisSim-a który nie zawiera implementacji wzorca obserwator

--- SenseSim-mathModel/src/main/java/org/mdyk/netsim/mathModel/communication/RoutingAlgorithm.java
+++ SenseSim-mathModel/src/main/java/org/mdyk/netsim/mathModel/communication/RoutingAlgorithm.java
@@ -1,8 +1,12 @@
 package org.mdyk.netsim.mathModel.communication;
 
 
+import org.mdyk.netsim.mathModel.sensor.ISensorModel;
+
+import java.util.List;
+
 public interface RoutingAlgorithm {
 
-
+    public List<ISensorModel<?>> getNodesToHop(ISensorModel<?> sender);
 
 }
