5ed3a74de540e4e3e66cfb2e6dadff245334cd8f
mdyk <mdyk@wat.edu.pl>
2016-01-28 22:56:01 +0100

    Wczytywanie zdjęc z pliku

--- SenseSim-mathModel/src/main/java/org/mdyk/netsim/mathModel/ability/AbilityType.java
+++ SenseSim-mathModel/src/main/java/org/mdyk/netsim/mathModel/ability/AbilityType.java
@@ -3,9 +3,12 @@ package org.mdyk.netsim.mathModel.ability;
 /**
  * Holds names for abilities
  */
+// TODO do usunięcia po wprowadzeniu obserwatora
 public enum AbilityType {
 
     TEMPERATURE,
+    PULSE,
+    BLOOD_SATURATION,
     PHOTO;
 
 }
