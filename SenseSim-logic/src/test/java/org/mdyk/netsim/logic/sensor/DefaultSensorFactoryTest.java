192d26527d30607e91d8e448e5150fe662ab9ca2
mdyk <mdyk@wat.edu.pl>
2016-03-08 15:49:08 +0100

    Test dla fabryki sensorów

---
+++ SenseSim-logic/src/test/java/org/mdyk/netsim/logic/sensor/DefaultSensorFactoryTest.java
@@ -0,0 +1,28 @@
+package org.mdyk.netsim.logic.sensor;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.mdyk.netsim.mathModel.sensor.SensorModel;
+
+public class DefaultSensorFactoryTest {
+
+    @Test
+    public void testBuildSensor() throws Exception {
+        DefaultSensorFactory sensorFactory = new DefaultSensorFactory();
+
+        SensorModel sensorModel = sensorFactory.buildSensor(PT100Sensor.class.getName());
+
+        TestCase.assertNotNull(sensorModel);
+        TestCase.assertTrue(sensorModel instanceof PT100Sensor);
+
+        boolean error = false;
+        try {
+            sensorFactory.buildSensor("AAA");
+        } catch (Exception exc) {
+            error = true;
+        }
+
+        TestCase.assertTrue(error);
+
+    }
+}
